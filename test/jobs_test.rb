781214e8756d35dbeacf2eb0b4be43d8f6a83874
mrstebo <mrstebouk@gmail.com>
2017-05-25 23:00:53 +0100

    Finished writing tests.

--- test/jobs_test.rb
+++ test/jobs_test.rb
@@ -6,4 +6,64 @@ class JobsTest < Minitest::Test
   def test_empty_sequence
     assert_equal '', Jobs.new.build_order('')
   end
+
+  def test_sequence_with_no_dependencies
+    skip
+    jobs = {
+      a: nil,
+      b: nil,
+      c: nil
+    }
+    assert_equal 'abc', Jobs.new.build_order(jobs)
+  end
+
+  def test_sequence_with_dependency
+    skip
+    jobs = {
+      a: nil,
+      b: :c,
+      c: nil
+    }
+    assert_equal 'acb', Jobs.new.build_order(jobs)
+  end
+
+  def test_sequence_with_dependencies
+    skip
+    jobs = {
+      a: nil,
+      b: :c,
+      c: :f,
+      d: :a,
+      e: :b,
+      f: nil
+    }
+    assert_equal 'fcbead', Jobs.new.build_order(jobs)
+  end
+
+  def test_sequence_with_self_dependency
+    skip
+    jobs = {
+      a: nil,
+      b: nil,
+      c: :c
+    }
+    assert_raises RuntimeError do
+      Jobs.new.build_order(jobs)
+    end
+  end
+
+  def test_sequence_with_circular_dependency
+    skip
+    jobs = {
+      a: nil,
+      b: :c,
+      c: :f,
+      d: :a,
+      e: nil,
+      f: :b
+    }
+    assert_raises RuntimeError do
+      Jobs.new.build_order(jobs)
+    end
+  end
 end
