77c0ce003240fa30349a9d5acab92f3f3746902a
mrstebo <mrstebouk@gmail.com>
2017-05-25 22:03:17 +0100

    Added code and test to check that an empty string returns no jobs.

--- lib/jobs.rb
+++ lib/jobs.rb
@@ -1,2 +1,6 @@
 class Jobs
+  def build_order(build_sequence)
+    return '' if build_sequence.empty?
+    build_sequence
+  end
 end
--- test/jobs_test.rb
+++ test/jobs_test.rb
@@ -3,4 +3,7 @@ require 'minitest/pride'
 require_relative '../lib/jobs'
 
 class JobsTest < Minitest::Test
+  def test_empty_sequence
+    assert_equal '', Jobs.new.build_order('')
+  end
 end
