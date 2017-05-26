ccd0c99205e442a67751d9eb34fbeb18087cc357
mrstebo <mrstebouk@gmail.com>
2017-05-26 20:10:06 +0100

    Updated the Jobs class to use the tsort module.

--- lib/jobs.rb
+++ lib/jobs.rb
@@ -1,32 +1,35 @@
+require 'tsort'
 require_relative 'circular_dependency_error'
 require_relative 'self_dependency_error'
 
 class Jobs
   def build_order(build_sequence)
     return '' if build_sequence.empty?
-    visited = []
-    stack = []
-
-    build_sequence.keys.each do |job|
-      next if visited.include?(job)
-      visited << job
-      find_dependencies(job, build_sequence).reverse.each do |dependency|
-        next if visited.include?(dependency)
-        visited << dependency
-        stack << dependency
-      end
-      stack << job
-    end
-
-    stack.map(&:to_s).join
+    hash = convert_to_tsortable_hash(build_sequence)
+    TSortableHash[hash].tsort.map(&:chr).join
   end
 
   private
 
-  def find_dependencies(job, sequence, visited=[])
-    return [] if sequence[job].nil?
-    raise SelfDependenyError, "#{job} cannot depend on itself" if job == sequence[job]
-    raise CircularDependenyError, "#{job} is a circular dependency" if visited.include?(sequence[job])
-    [sequence[job]] + find_dependencies(sequence[job], sequence, visited + [job])
+  def convert_to_tsortable_hash(build_sequence)
+    build_sequence.inject({}) do |x, (k, _)|
+      x[k.to_s.ord] = find_dependencies(k, build_sequence).map(&:to_s).map(&:ord)
+      x
+    end
+  end
+
+  def find_dependencies(job, build_sequence, visited=[])
+    return [] if build_sequence[job].nil?
+    raise SelfDependenyError, "#{job} cannot depend on itself" if job == build_sequence[job]
+    raise CircularDependenyError, "#{job} is a circular dependency" if visited.include?(build_sequence[job])
+    [build_sequence[job]] + find_dependencies(build_sequence[job], build_sequence, visited + [job])
+  end
+
+  class TSortableHash < Hash
+    include TSort
+    alias tsort_each_node each_key
+    def tsort_each_child(node, &block)
+      fetch(node).each(&block)
+    end
   end
 end
