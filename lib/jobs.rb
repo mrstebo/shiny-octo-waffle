bff2923c588d07235c0c95267a61ba917d4d2c62
mrstebo <mrstebouk@gmail.com>
2017-05-26 23:15:29 +0100

    Changed input to a string of jobs, with each job on a new line.

--- lib/jobs.rb
+++ lib/jobs.rb
@@ -3,23 +3,27 @@ require_relative 'circular_dependency_error'
 require_relative 'self_dependency_error'
 
 class Jobs
-  def build_order(build_sequence)
-    return '' if build_sequence.empty?
-    hash = convert_to_tsortable_hash(build_sequence)
+  def build_order(sequence)
+    sequence_hash = convert_to_hash(sequence)
+    hash = convert_to_tsortable_hash(sequence_hash)
     TSortableHash[hash].tsort.map(&:chr).join
   end
 
   private
 
+  def convert_to_hash(build_sequence)
+    Hash[build_sequence.lines.map {|line| line.split('=>').map(&:strip)}]
+  end
+
   def convert_to_tsortable_hash(build_sequence)
     build_sequence.inject({}) do |x, (k, _)|
-      x[k.to_s.ord] = find_dependencies(k, build_sequence).map(&:to_s).map(&:ord)
+      x[k.ord] = find_dependencies(k, build_sequence).map(&:ord)
       x
     end
   end
 
   def find_dependencies(job, build_sequence, visited=[])
-    return [] if build_sequence[job].nil?
+    return [] if build_sequence[job].empty?
     raise SelfDependenyError, "#{job} cannot depend on itself" if job == build_sequence[job]
     raise CircularDependenyError, "#{job} is a circular dependency" if visited.include?(build_sequence[job])
     [build_sequence[job]] + find_dependencies(build_sequence[job], build_sequence, visited + [job])
